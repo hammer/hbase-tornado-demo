cf633ad23f880079d678b5b08c43e3981ac1b7e9
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2010-06-17 15:59:12 -0700

    Get Tornado talking to HBase trunk over Avro via pyhbase.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 A demonstration of [HBase](http://hbase.org) and [Tornado](http://www.tornadoweb.org).
+
+Adds new features to the chat demo:
+
+(none implemented yet)
+
+Upcoming features:
+1. Persist chat messages to HBase.
+1. Bots
+1. Channels
+
--- site.py
+++ site.py
@@ -1,45 +1,160 @@
-import os
+#!/usr/bin/env python
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
+import logging
+import tornado.auth
+import tornado.escape
 import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
+import os.path
+import uuid
 
-from pyhbase import HBaseConnection
+from tornado.options import define, options
+from pyhbase.connection import HBaseConnection
 
-class HBaseTable(object):
-  def __init__(self, name):
-    self._name = name
-    self._conn = HBaseConnection('localhost', 9090)
+define("port", default=8888, help="run on the given port", type=int)
 
-  def get_column_families(self):
-    column_descriptors = self._conn.client.getColumnDescriptors(self.name)
-    return [cd for cd in column_descriptors.values()]
 
-  def get_regions(self):
-    regions = self._conn.client.getTableRegions(self.name)
-    return regions
-  
-  # read-only properties
-  name = property(lambda self: self._name)
-  column_families = property(lambda self: self.get_column_families())
-  regions = property(lambda self: self.get_regions())
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/auth/login", AuthLoginHandler),
+            (r"/auth/logout", AuthLogoutHandler),
+            (r"/a/message/new", MessageNewHandler),
+            (r"/a/message/updates", MessageUpdatesHandler),
+        ]
+        settings = dict(
+            cookie_secret="43oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            login_url="/auth/login",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
-class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-    conn = HBaseConnection('localhost', 9090)
-    tables = [HBaseTable(t) for t in conn.client.getTableNames()]
-    self.render("index.html", tables=tables)
 
-settings = {
-  "static_path": os.path.join(os.path.dirname(__file__), "static"),
-  "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-}
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        user_json = self.get_secure_cookie("user")
+        if not user_json: return None
+        return tornado.escape.json_decode(user_json)
 
-application = tornado.web.Application([
-  (r"/", MainHandler),
-], **settings)
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        hbase_conn = HBaseConnection('localhost', 9090)
+        hbase_version = hbase_conn.get_hbase_version()
+        self.render("index.html", messages=MessageMixin.cache, hbase_version=hbase_version)
+
+
+class MessageMixin(object):
+    waiters = []
+    cache = []
+    cache_size = 200
+
+    def wait_for_messages(self, callback, cursor=None):
+        cls = MessageMixin
+        if cursor:
+            index = 0
+            for i in xrange(len(cls.cache)):
+                index = len(cls.cache) - i - 1
+                if cls.cache[index]["id"] == cursor: break
+            recent = cls.cache[index + 1:]
+            if recent:
+                callback(recent)
+                return
+        cls.waiters.append(callback)
+
+    def new_messages(self, messages):
+        cls = MessageMixin
+        logging.info("Sending new message to %r listeners", len(cls.waiters))
+        for callback in cls.waiters:
+            try:
+                callback(messages)
+            except:
+                logging.error("Error in waiter callback", exc_info=True)
+        cls.waiters = []
+        cls.cache.extend(messages)
+        if len(cls.cache) > self.cache_size:
+            cls.cache = cls.cache[-self.cache_size:]
+
+
+class MessageNewHandler(BaseHandler, MessageMixin):
+    @tornado.web.authenticated
+    def post(self):
+        message = {
+            "id": str(uuid.uuid4()),
+            "from": self.current_user["first_name"],
+            "body": self.get_argument("body"),
+        }
+        message["html"] = self.render_string("message.html", message=message)
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next"))
+        else:
+            self.write(message)
+        self.new_messages([message])
+
+
+class MessageUpdatesHandler(BaseHandler, MessageMixin):
+    @tornado.web.authenticated
+    @tornado.web.asynchronous
+    def post(self):
+        cursor = self.get_argument("cursor", None)
+        self.wait_for_messages(self.async_callback(self.on_new_messages),
+                               cursor=cursor)
+
+    def on_new_messages(self, messages):
+        print "NEW MESSAGE"
+        # Closed client connection
+        if self.request.connection.stream.closed():
+            return
+        self.finish(dict(messages=messages))
+
+
+class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
+    @tornado.web.asynchronous
+    def get(self):
+        if self.get_argument("openid.mode", None):
+            self.get_authenticated_user(self.async_callback(self._on_auth))
+            return
+        self.authenticate_redirect(ax_attrs=["name"])
+    
+    def _on_auth(self, user):
+        if not user:
+            raise tornado.web.HTTPError(500, "Google auth failed")
+        self.set_secure_cookie("user", tornado.escape.json_encode(user))
+        self.redirect("/")
+
+
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.write("You are now logged out")
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
-  http_server = tornado.httpserver.HTTPServer(application)
-  http_server.listen(8888)
-  tornado.ioloop.IOLoop.instance().start()
+    main()
